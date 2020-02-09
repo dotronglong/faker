a0b68ab760e05692f9e4dfcd627cbcac0e6ea8ec
Lucas <me@dotronglong.com>
2020-02-09 15:43:30 +0800

    Rebuild in Kotlin

---
+++ src/main/kotlin/com/dotronglong/faker/controller/MainController.kt
@@ -0,0 +1,7 @@
+package com.dotronglong.faker.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController {
+}
---
+++ src/test/kotlin/com/dotronglong/faker/FakerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dotronglong.faker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FakerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
