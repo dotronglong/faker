fea6518f33bbb6c403b917411104bfc0181b0fcc
Lucas <me@dotronglong.com>
2020-02-13 15:26:27 +0800

    Add version

--- src/main/kotlin/com/dotronglong/faker/FakerApplication.kt
+++ src/main/kotlin/com/dotronglong/faker/FakerApplication.kt
@@ -1,8 +1,11 @@
 package com.dotronglong.faker
 
+import com.dotronglong.faker.controller.MainController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
+@Import(value = [MainController::class])
 @SpringBootApplication
 class FakerApplication
 
